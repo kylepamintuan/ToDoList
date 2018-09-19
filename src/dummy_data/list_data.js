e5edb426fafbc352da1ae5959d6f3ef3bcfb089a
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-09-19 15:05:22 -0700

    finished displaying dummy data.

---
+++ src/dummy_data/list_data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'Go to Store',
+        details: 'Buy air freshner for car',
+        _id: '001'
+    },
+    {
+        title: 'Wash Car',
+        details: 'Go to quarter wash on Pioneer & Del Amo',
+        _id: '002'
+    },
+    {
+        title: 'Collect Camping Gear',
+        details: 'Gather all list items on Zion camping list',
+        _id: '003'
+    },
+    {
+        title: 'Laundry',
+        details: 'Wash darks and lights',
+        _id: '004'
+    },
+    {
+        title: 'Finish prototypes',
+        details: 'Complete front-end and back-end prototypes',
+        _id: '005'
+    }
+]
